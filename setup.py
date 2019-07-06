4f4ea43bce28ddad895647658ccfa6a6a9f06ba1
ucrux <ucrux@example.com>
2019-07-06 19:53:30 +0800

    添加修改视频元数据的方法

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 setup(name='aioffmpeg',
-      version='0.1.8',
+      version='0.1.9',
       description='a ffmpeg wrapped lib for asyncio',
       long_description='',
       author='ucrux',
