010bd14e1a8ea01297733d9f65e6c8d52db7281b
ucrux <ucrux@example.com>
2019-07-30 15:12:11 +0800

    scale video add cuvid cuda decoder support

--- tests/test_scale_video_cuda.py
+++ tests/test_scale_video_cuda.py
@@ -22,7 +22,7 @@ async def test_scale_video_cuda_aio():
                                                    target_height=random.randint(300, 1000),
                                                    target_videobitrate=random.randint(100, 400),
                                                    encode_lib=H264EncoderArgs.codec_v_h264_nvenc,
-                                                   hwaccel=H264EncoderArgs.hwaccel_cuvid,
+                                                   hwaccel=H264EncoderArgs.hwaccel_cuda,
                                                    decoder=H264EncoderArgs.decoder_h264_cuvid)
     assert scaled_obj is not None and stderr == ''
     print('H264Video object info:', scaled_obj)
@@ -45,7 +45,7 @@ def test_scale_video_cuda():
                                          target_height=random.randint(300, 1000),
                                          target_videobitrate=random.randint(100, 400),
                                          encode_lib=H264EncoderArgs.codec_v_h264_nvenc,
-                                         hwaccel=H264EncoderArgs.hwaccel_cuvid,
+                                         hwaccel=H264EncoderArgs.hwaccel_cuda,
                                          decoder=H264EncoderArgs.decoder_h264_cuvid,
                                          ac=H264EncoderArgs.audio_channel_1,ar=H264EncoderArgs.audio_simple_rate_48000)
     assert scaled_obj is not None and stderr == ''
